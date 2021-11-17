649e1004ed3004f6b9900ed2e25f6134468d0813
Thamara Chellakooty <thamaralinnea@gmail.com>
2021-11-17 20:34:57 +0100

    App icon added

--- app/src/main/java/com/example/s205337lykkehjulet/MainActivity.kt
+++ app/src/main/java/com/example/s205337lykkehjulet/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.s205337lykkehjulet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.s205337lykkehjulet.databinding.ActivityMainBinding
+
+/**
+ * https://developer.android.com/guide/navigation/navigation-getting-started#kotlin
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
