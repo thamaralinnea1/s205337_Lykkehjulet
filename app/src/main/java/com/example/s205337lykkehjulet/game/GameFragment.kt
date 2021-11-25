9e301de0a38f3c4be30da5cd1f32965ff19d7333
Thamara Chellakooty <thamaralinnea@gmail.com>
2021-11-25 22:48:12 +0100

    secondfragment and its viewholderclass

---
+++ app/src/main/java/com/example/s205337lykkehjulet/game/GameFragment.kt
@@ -0,0 +1,47 @@
+package com.example.s205337lykkehjulet.game
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.example.s205337lykkehjulet.R
+import com.example.s205337lykkehjulet.databinding.FragmentStartBinding
+import kotlinx.android.synthetic.main.fragment_second_fragment.view.*
+
+class GameFragment : Fragment() {
+    private val viewModel: GameViewHolder by viewModels()
+
+    private var binding: FragmentStartBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_second_fragment, container, false)
+
+        //Tilfældigt valg ord:
+        val category = context?.resources?.getString(requireArguments().getInt("Title"))
+
+        viewModel.generateWord(category!!)
+
+        root.findViewById<TextView>(R.id.testTxt).text = viewModel.word
+        return root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            // Setter en click listner på knapperne i secondfragment
+            root.kob_vokal.setOnClickListener { activateWheel() }
+        }
+    }
+
+    fun activateWheel () {
+        Toast.makeText(activity,"Hjul drejer", Toast.LENGTH_SHORT).show()
+    }
+
+}
+
--- app/src/main/java/com/example/s205337lykkehjulet/game/GameViewHolder.kt
+++ app/src/main/java/com/example/s205337lykkehjulet/game/GameViewHolder.kt
@@ -1,14 +1,46 @@
 package com.example.s205337lykkehjulet.game
 
-class GameViewHolder {
-    var word: String = "null"
-    constructor() {
-        //TODO: Make random
-        word = "test"
+import androidx.lifecycle.ViewModel
+import com.example.s205337lykkehjulet.data.dyr
+
+
+class GameViewHolder: ViewModel() {
+    private val wordList: MutableList<String> = mutableListOf()
+    // lateint betyder at variablen først definneres senere
+    private lateinit var currentWord: String
+
+    private fun NewWord() {
+        currentWord = dyr.random()
     }
 
-    fun generateWord () {
-        word = " test"
+
+
+    var word: String = "null"
+
+
+    fun generateWord(category: String): String {
+        when (category) {
+            "Dyr" -> {
+                word = "Kategori Dyr"
+                return word
+            }
+            "Handlinger" -> {
+                word = " Kategori Handlinger"
+                return word
+            }
+            "Ordsprog" -> {
+                word = " Kategori Ordsprog"
+                return word
+            }
+            "Udtryk" -> {
+                word = "Kategori Udtryk"
+                return word
+            }
+            else -> {
+                word = "Intet"
+                return word
+            }
+        }
     }
 
 }
