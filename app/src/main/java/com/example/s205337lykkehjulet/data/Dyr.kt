0b037e78d4b4a0577f7781062e9acfce8cc88751
Thamara Chellakooty <thamaralinnea@gmail.com>
2021-11-25 22:47:11 +0100

    Data class with a list of  strings for the "dyr" category

---
+++ app/src/main/java/com/example/s205337lykkehjulet/data/Dyr.kt
@@ -0,0 +1,12 @@
+package com.example.s205337lykkehjulet.data
+
+
+
+
+val dyr: List<String> =
+    listOf(
+        "elefant",
+        "kanin",
+        "guldfisk",
+        "leopard",
+        "panda")
