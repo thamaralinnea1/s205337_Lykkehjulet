759b2f597b49a87dd069f3180519534ccf9ac56b
Thamara Chellakooty <thamaralinnea@gmail.com>
2021-11-27 20:28:11 +0100

    setOnclicklistner changed to cardview instead of button

--- app/src/main/java/com/example/s205337lykkehjulet/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/s205337lykkehjulet/adapter/ItemAdapter.kt
@@ -7,23 +7,28 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.navigation.compose.rememberNavController
+import androidx.cardview.widget.CardView
+import androidx.core.os.bundleOf
+import androidx.navigation.Navigation
+
 import androidx.recyclerview.widget.RecyclerView
 import com.example.s205337lykkehjulet.R
 import com.example.s205337lykkehjulet.model.ImageButtonCategories
 
-class ItemAdapter(private val context: Context, private val dataset: List<ImageButtonCategories>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+class ItemAdapter(private val context: Context, private val dataset: List<ImageButtonCategories>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
         val imageView: ImageView = view.findViewById(R.id.category_image)
-        val button: Button = view.findViewById(R.id.category_button)
+        val cardView: CardView = view.findViewById(R.id.cardview)
+        //val button: Button = view.findViewById(R.id.category_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         // create a new view
-        val adapterLayout = LayoutInflater.from(parent.context). inflate(R.layout.list_item, parent, false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
@@ -32,9 +37,10 @@ class ItemAdapter(private val context: Context, private val dataset: List<ImageB
         holder.textView.text = context.resources.getString(item.stringResourceId)
         holder.imageView.setImageResource(item.imageResourceId)
 
-        holder.button.setOnClickListener {
-
-            Toast.makeText(context, "test " + context.resources.getString(item.stringResourceId), Toast.LENGTH_SHORT).show()
+        holder.cardView.setOnClickListener {
+            val bundle = bundleOf("Title" to item.stringResourceId)
+            Navigation.findNavController(it).navigate(R.id.action_start_to_second_fragment, bundle)
+            //   Toast.makeText(context, "test " + context.resources.getString(item.stringResourceId), Toast.LENGTH_SHORT).show()
         }
     }
     override fun getItemCount(): Int{
