3e206f1e5358f3f8159b6a431dc3cbb4f27e8d1a
Thamara Chellakooty <thamaralinnea@gmail.com>
2021-11-28 01:09:58 +0100

    List with wheel fields created

---
+++ app/src/main/java/com/example/s205337lykkehjulet/data/Wheel.kt
@@ -0,0 +1,23 @@
+package com.example.s205337lykkehjulet.data
+
+
+
+val fieldList: List<String> =
+    listOf(
+        "Ekstra Liv",
+        "800 point",
+        "750 point",
+        "Mistet Liv",
+        "250 point",
+        "1000 point",
+        "Bankerot",
+        "500 point",
+        "Ekstra Liv",
+        "800 point",
+        "750 point",
+        "Mistet Liv",
+        "250 point",
+        "1000 point",
+        "Bankerot",
+        "500 point")
+
